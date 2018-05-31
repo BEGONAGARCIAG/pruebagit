d400187d473e5acaee156d58f6994188b310e42f
BEGONAGARCIAG <begonag@yhaoo.com>
2018-05-31 13:35:35 +0200

    Initial commit

---
+++ src/main/java/com/asefa/pruebagit/App.java
@@ -0,0 +1,13 @@
+package com.asefa.pruebagit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
